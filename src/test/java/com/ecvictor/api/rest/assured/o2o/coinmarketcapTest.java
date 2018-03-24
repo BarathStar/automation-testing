24dfa90901d3c21288d02c650d536ce6ec347b73
Charles Cao <caotaotao@gmail.com>
2018-03-24 16:07:59 -0400

    coinmarketcapTest config file

--- src/test/java/com/ecvictor/api/rest/assured/o2o/coinmarketcapTest.java
+++ src/test/java/com/ecvictor/api/rest/assured/o2o/coinmarketcapTest.java
@@ -1,7 +1,14 @@
 package com.ecvictor.api.rest.assured.o2o;
 
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static com.ecvictor.api.rest.assured.o2o.RestAssuredUtil.getProperties;
 import static io.restassured.RestAssured.expect;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -11,6 +18,12 @@ import static org.hamcrest.core.IsEqual.equalTo;
  * Copyright (c) 2015 Service ECVictor Inc. All rights reserved.
  */
 public class coinmarketcapTest {
+    @BeforeClass
+    public static void  setup(){
+        Properties prop = getProperties();
+        RestAssured.baseURI =prop.getProperty("coinmarketcap.url");
+    }
+
     /**
      * Should not be able to access.
      */
@@ -18,9 +31,18 @@ public class coinmarketcapTest {
     public void coinmarket_top_10_coin_returns_200_with_expected_limit_and_unit() {
         expect().statusCode(200)
                 .given().log().uri()
-                .when().get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
+                .when().get("/ticker/?limit=10")
                 .then().log().ifValidationFails()
-                .body("size()", lessThan(10)).body("name", hasItem("Bitcoin"))
+                .body("size()", lessThan(11)).body("name", hasItem("Bitcoin"))
+                .contentType("application/json");
+    }
+    @Test
+    public void coinmarket_top_global_returns_200_with_expected_limit_and_unit() {
+        expect().statusCode(200)
+                .given().log().uri()
+                .when().get("/global")
+                .then().log().ifValidationFails()
+                .body("bitcoin_percentage_of_market_cap", notNullValue())
                 .contentType("application/json");
     }
 
